10bc08eb777cef80c2ba8893f383b3cb42447f95
Sachintha Waduge <isurusachin123@gmail.com>
2025-04-22 15:52:28 +0530

    update middleware

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -6,7 +6,7 @@ const authenticateJWT = (req, res, next) => {
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded; // Add the user ID to the request object
+    req.user = decoded; 
     next();
   } catch (err) {
     res.status(400).json({ message: 'Invalid or expired token' });
